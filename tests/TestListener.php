ee6e33ed56474791d90962fa7b478bcee5ef82f5
Daniel Archer <contato@danielcramer.com.br>
2020-08-15 00:07:48 -0400

    reset database before start

--- tests/TestListener.php
+++ tests/TestListener.php
@@ -2,19 +2,17 @@
 
 namespace MyCertsTests;
 
-use PHPUnit\Runner\BeforeFirstTestHook;
 use PHPUnit\Runner\AfterLastTestHook;
+use PHPUnit\Runner\BeforeFirstTestHook;
 
 final class TestListener implements BeforeFirstTestHook, AfterLastTestHook
 {
     public function executeBeforeFirstTest(): void
     {
-
+        echo shell_exec('php /var/www/lumen/artisan migrate:refresh --seed -q');
     }
 
     public function executeAfterLastTest(): void
     {
-        shell_exec('php lumen/artisan migration:refresh --seed');
-        echo 'Database refreshed...';
     }
 }
